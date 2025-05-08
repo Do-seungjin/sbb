1289d74c4a29a87c9d8b157489b7fa3483ae2841
Do-seungjin <dosjin01@gmail.com>
2025-05-08 16:37:33 +0900

    [도승진]UserRole 파일, UserSecurityService 생성

--- sbb/src/main/java/org/kosa/sbb/user/UserRepository.java
+++ sbb/src/main/java/org/kosa/sbb/user/UserRepository.java
@@ -1,7 +1,8 @@
 package org.kosa.sbb.user;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<SiteUser, Long> {
-  
+  Optional<SiteUser> findByusername(String username);
 }
