41281f178839f2d8aec3fb394c67096546ed27d9
Do-seungjin <dosjin01@gmail.com>
2025-05-08 11:30:23 +0900

    [도승진]회원 가입 기능

--- sbb/src/main/java/org/kosa/sbb/user/UserService.java
+++ sbb/src/main/java/org/kosa/sbb/user/UserService.java
@@ -9,12 +9,12 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
 
   private final UserRepository userRepository;
-  
+  private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
   public SiteUser create(String username, String email, String password) {
     SiteUser user = new SiteUser();
     user.setUsername(username);
     user.setEmail(email);
-    BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
     user.setPassword(passwordEncoder.encode(password));
     userRepository.save(user);
     return user;
