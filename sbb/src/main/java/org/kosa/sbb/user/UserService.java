73e8a350aee576dd18c7285b4073126a521ed408
Do-seungjin <dosjin01@gmail.com>
2025-05-08 14:31:36 +0900

    [도승진]회원 생성 유효성 검사 폼 생성

--- sbb/src/main/java/org/kosa/sbb/user/UserService.java
+++ sbb/src/main/java/org/kosa/sbb/user/UserService.java
@@ -1,17 +1,18 @@
 package org.kosa.sbb.user;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
   private final UserRepository userRepository;
-  private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+  private final PasswordEncoder passwordEncoder;
 
   public SiteUser create(String username, String email, String password) {
+    
     // 회원 정보 객체 생성
     SiteUser user = SiteUser.of(username, passwordEncoder.encode(password), email);
     // 저장소에 회원 정보 등록
